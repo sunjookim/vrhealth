29d4109fa9c6e29392c344b77e9c27c8ee824ddb
DESKTOP-IPPO8CG\sunjoo <tjswn7051@naver.com>
2018-08-28 23:23:10 +0900

    0828

---
+++ games_0828/Assets/Sleigh/Scripts/Player.cs
@@ -0,0 +1,253 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+
+namespace Ardunity
+{
+
+    public class Player : MonoBehaviour
+    {
+        private GameUI UIObj;      // ui 오브젝트
+        private GameObject[] img;
+        private StepCtrl stepCtrl;
+
+        public float x;     // 마우스에 따른 x회전 위치
+        public int cornerCount = 1;
+
+        public GameObject snowing; // 눈내리는 이벤트
+        private GameObject cornertr;
+        public GameObject BoostEf; // 부스터 이펙트
+        private GameObject newEf;
+
+        NavMeshAgent nvAgent;
+
+        public float nvSpeed, pSpeed; // 플레이어 속도, 네비 속도
+        public bool isColl, isBoost, isCollEnemy;
+        public bool isSt1, isSt2, isSt3;
+        private int Life;
+        private float countTime;
+
+        public Renderer lamp;
+        private Color originColor; // 원래 색깔
+
+        // Use this for initialization
+        void Start()
+        {
+            originColor = lamp.material.color;
+            // 네비게이션
+            nvAgent = gameObject.GetComponent<NavMeshAgent>();
+            nvAgent.speed = nvSpeed; // 코너를 따라가는 속도
+
+            UIObj = GameObject.Find("Game_UI").GetComponent<GameUI>(); // GameUI 오브젝트 찾음
+            UIObj.ChangeCorner(5 - cornerCount);
+            img = GameObject.FindGameObjectsWithTag("img"); // 이미지 UI 오브젝트들
+            Life = img.Length;
+            Debug.Log("목숨 : " + Life);
+            isSt1 = false; isSt2 = false; isSt3 = false; // 스텝 1,2,3은 다 비활성 상태
+            isBoost = false; isColl = false;
+
+            stepCtrl = GetComponent<StepCtrl>();
+
+            nvSpeed = 4f;
+            pSpeed = 0.17f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Life <= 0)
+            {
+                // 게임 오버 메시지 뜨게 함- 목숨을 다 소모했다고 함
+                UIObj.ChangeGameOver(0);
+            }
+            else
+            {
+                //RotationReactor2 snowsnow = GameObject.Find("snow").GetComponent<RotationReactor2>(); // 나중에 형이 바꿔줌
+
+                if (cornerCount != 5) // 코너를 다 돌지 않았으면 계속 코너를 따라가도록 함
+                {
+                    cornertr = GameObject.FindWithTag("corner" + cornerCount);
+                    nvAgent.SetDestination(cornertr.transform.position);
+                    nvAgent.speed = nvSpeed; // 코너를 따라가는 속도
+                }
+                // 눈 이벤트 위치 조정
+                Vector3 vector3 = new Vector3(transform.position.x, transform.position.y + 30, transform.position.z);
+                snowing.transform.position = vector3;
+
+                if(isCollEnemy)
+                {
+                    float flicker = Mathf.Abs(Mathf.Sin(Time.time * 10));
+                    lamp.material.color = originColor * flicker;
+                }
+
+                if (!isColl) // 충돌하지 않으면
+                {
+                    //플레이어 회전 구현
+                    // 코너 1 : -90~90, 코너 2 : 0~180 ....
+                    x = (Input.mousePosition.x - Screen.width / 2) / Screen.width; // x의 범위는 -0.5~ 0.5
+
+                    // 회전반경 제한
+                    if (x < -0.5f) x = -0.5f; if (x > 0.5f) x = 0.5f;
+
+                    Quaternion turn = Quaternion.Euler(0, x * 180 + 90 * (cornerCount - 1), 0);
+                    transform.rotation = turn;
+                    //transform.rotation = Quaternion.Slerp(transform.rotation, turn, 7 * Time.deltaTime); // 부드럽게 턴
+                    transform.Translate(Vector3.forward * pSpeed); // 가속
+
+                    if (!isBoost) // 부스터가 아님
+                    {
+                        pSpeed *= 0.996f;
+                        nvSpeed *= 0.996f;
+                    }
+                    else // 부스터 상태
+                    {
+                        if (newEf) // 오브젝트가 할당 된 상태야 함, 부스터 이펙트 생성
+                        {
+                            newEf.transform.position = transform.position;
+                            newEf.transform.Translate(new Vector3(0, 2, 0));
+                        }
+
+                        pSpeed *= 1.2f;
+                        nvSpeed *= 1.2f;
+
+                        if (pSpeed >= 0.3f) pSpeed = 0.3f;
+                        if (nvSpeed >= 4f) nvSpeed = 4f;
+                    }
+                }
+
+                if(Time.time <= countTime) // 제한 시간안에 올바른 동작을 함
+                {
+
+                }
+                else // 동작을 수행하지 못함 : 목숨 깎이고 실패하는 애니메이션 - 미끄러지는 애니메이션 말고, 속도는 그대로
+                {
+
+                }
+
+                if (Input.GetKeyDown(KeyCode.Q) && !isSt1) // Time.time <= countTime 나중에 이것도 추가 - 제한 시간안에 올바른 동작을 함
+                {
+                    stepCtrl.StepOne();
+                    isSt1 = true;
+                    
+                }
+                else if (Input.GetKeyDown(KeyCode.W) && !isSt2)
+                {
+                    stepCtrl.StepTwo();
+                    isSt2 = true;
+                    
+                }
+                else if (Input.GetKeyDown(KeyCode.E) && !isSt3)
+                {
+                    stepCtrl.StepThree();
+                    isSt3 = true;
+                    
+                }
+
+                /**** 밑에는 아두니티 쓰는거 *****/
+                //if (snowsnow.snow0)
+                //{
+                //    stepCtrl.StepOne();
+                //}
+                //else if (snowsnow.snow1)
+                //{
+                //    stepCtrl.StepTwo();
+                //}
+                //else if (snowsnow.snow2)
+                //{
+                //    stepCtrl.StepThree();
+                //}
+                else if (Input.GetKeyDown(KeyCode.R))
+                {
+                    stepCtrl.StepSuccess();
+                }
+                else if (Input.GetKeyDown(KeyCode.T))
+                {
+                    stepCtrl.StepFail();
+                }
+            }
+        }
+
+        void OnCollisionEnter(Collision coll) // 부딪혔을 때
+        {
+            if (coll.collider.tag == ("corner" + cornerCount))
+            {
+                isColl = true;            // 부딪힌 상태
+                Destroy(coll.gameObject); // 코너 삭제
+                
+                cornerCount++;
+                UIObj.GetComponent<GameUI>().ChangeCorner(5 - cornerCount);
+
+                if(cornerCount < 5)
+                {
+                    Quaternion turn = Quaternion.Euler(0, x * 180 + 30 * (cornerCount - 1), 0);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, turn, 0.01f * Time.deltaTime); // 부드럽게 턴
+                    Invoke("isCollFalse", 2f); // 3초 후에 정상
+                }
+                else
+                {
+                    // 맵을 다 돌았으니까 최종 결과가 나오고, 만약 최고기록보다 짧게 들어오면 갱신함
+                    UIObj.ChangeGameOver(1);
+                    UIObj.ChangeBest();
+                }
+            }
+
+            if (coll.collider.tag == ("enemy"))
+            {
+                isColl = true;            // 부딪힌 상태
+                isCollEnemy = true;
+                // 목숨 깎이고, 뱅글돌아가는 효과 호출
+                nvSpeed = 0f;
+                pSpeed = 0f;
+
+                Debug.Log("적이랑 부딪힘 ㅜㅜ : life" + Life + "번 깎임");
+                Destroy(GameObject.Find("life" + Life));
+                Life--;
+
+                stepCtrl.StepFail();
+                Invoke("isCollFalse", 3f); // 3초 후에 정상
+                Clear();
+            }
+        }
+
+        void isCollFalse() // 코너 부딪힘 끝
+        {
+            Debug.Log("isColl false!");
+            isColl = false;
+            isCollEnemy = false;
+            lamp.material.color = originColor;
+        }
+
+        public void Booster() // 부스터 온
+        {
+            isBoost = true;
+            Debug.Log("부스터 온!");
+            newEf = Instantiate(BoostEf, this.transform.position, this.transform.rotation) as GameObject;
+
+            nvSpeed = 1f;
+            pSpeed = 0.10f;
+            Destroy(newEf, 1.5f); // 1.5초뒤에 이펙트 삭제
+            Invoke("BoostOff", 1.5f); // 1.5초 후에 정상
+        }
+
+        void BoostOff() // 부스터 끄기
+        {
+            Debug.Log("부스터 오프!");
+            isBoost = false;
+        }
+
+        void CountDown() // 동작을 실행하기 위해 시간을 초기화
+        {
+            countTime = Time.time + 5f; // 5초 추가
+        }
+
+        public void Clear() // 불값 초기화
+        {
+            isSt1 = false;
+            isSt2 = false;
+            isSt3 = false;
+        }
+    }
+}
---
+++ games_0828/Assets/Sleigh/Scripts/RotationReactor2S.cs
@@ -0,0 +1,264 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+
+
+
+
+namespace Ardunity
+{
+    
+
+        [AddComponentMenu("ARDUnity/Reactor/Transform/RotationReactor2S")]
+    [HelpURL("https://sites.google.com/site/ardunitydoc/references/reactor/rotationreactor")]
+	public class RotationReactor2S : ArdunityReactor
+    {
+
+        
+         
+        
+        public Animator animator;
+        public bool smoothFollow = true;
+
+        float time = 0.0f;
+        float time2 = 0.0f;
+		private Quaternion _initRot;
+		private IWireInput<Quaternion> _rotation;
+        private Quaternion _curRotation;
+		private Quaternion _fromRotation;
+		private Quaternion _toRotation;
+		private float _time;
+        private Quaternion a;
+        private bool k = true;
+        private int count = 0;
+        private int count1 = 0;
+        private int decision = 0;
+        public bool rightback = false;
+        public bool leftback = false;
+        public bool snow0,snow1,snow2 = false;
+
+        protected override void Awake()
+		{
+            base.Awake();
+			
+			_initRot = transform.localRotation;
+            _curRotation = Quaternion.identity;
+            _toRotation =  Quaternion.identity;
+                        
+            _time = 1f;
+            
+        }
+       
+
+        
+        // Use this for initialization
+        void Start ()
+		{
+            
+            
+            animator = GetComponent<Animator>();
+           
+            
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+
+            time += Time.deltaTime;
+            
+
+            if (_time < 1f && smoothFollow == true)
+            {
+                _time += Time.deltaTime;
+                _curRotation = Quaternion.Lerp(_fromRotation, _toRotation, _time);
+            }
+            else
+                _curRotation = _toRotation;
+            //  print(_curRotation.x);
+           
+            Debug.Log("2번"+"x:"+_curRotation.x+" / "+"y:"+_curRotation.y+" / "+"z:"+_curRotation.z+" / "+"w:"+_curRotation.w);
+
+            RotationReactorS _curRotation2 = GameObject.Find("snow").GetComponent<RotationReactorS>(); // 1번
+            
+            RotationReactor4 _curRotation4 = GameObject.Find("snow").GetComponent<RotationReactor4>(); // 4번
+
+            //print(_curRotation2._curRotation);
+            //print(_curRotation3._curRotation);
+            if (_curRotation.x != 0)
+            {
+                count++;
+            }
+            if (count == 800)
+            {
+                a = _curRotation;
+                //print(a);
+                //transform.localRotation = _initRot * _curRotation;
+                // k = false;
+                
+
+            }
+            Debug.Log("2번초기값" + "x:" + a.x + " / " + "y:" + a.y + " / " + "z:" + a.z + " / " + "w:" + a.w);
+            //print(a);
+
+
+
+            
+                   
+
+           if (time >= 3.5f) 
+               {
+                    if (decision == 0) // 팔운동1 
+                    {
+                        if (Math.Abs(_curRotation2._curRotation.y) < 0.8f && Math.Abs(_curRotation2._curRotation.y) > 0.4f      // 1번 y: 0.4 < 현재값 < 0.8
+                                && Math.Abs(_curRotation4._curRotation.y) < 0.8f && Math.Abs(_curRotation4._curRotation.y) > 0.4f) // 4번 y: 0.4 < 현재값 < 0.8
+                        {
+
+                            //animator.Play("Makehuman_gameskel|gun_game_1_bullet", -1, 0);  // 팔운동1 애니메이션 실행
+                            count1++;
+                            decision++;  // 팔운동 1 애니메이션이 실행되면 팔운동2 애니메이션이 실행될수있도록 decision = 1로 바꿔준다.
+                            snow0 = true;
+                            snow2 = false;
+                       
+                        }
+                    }
+               }
+
+           else { }
+
+            print("1번" + snow0);
+
+           print("카운트값1: " + decision);
+
+
+               if (time >= 3.5f) 
+               {
+               if (decision == 1) // 팔운동2
+               {
+
+                    if (Math.Abs(_curRotation2._curRotation.x) < 0.8f && Math.Abs(_curRotation2._curRotation.x) > 0.4f && Math.Abs(_curRotation2._curRotation.y) < 0.3f     // 1번 x: 0.4 < 현재값 < 0.8  y : 현재값 <0.3f
+                         && Math.Abs(_curRotation4._curRotation.x) < 0.8f && Math.Abs(_curRotation4._curRotation.x) > 0.4f) // 4번 x: 0.4 < 현재값 < 0.8
+                      
+                   {   // and 3번 : 현재값 < 0.15f  and 4번 : 현재값 > 0.3f 
+
+                        //animator.Play("Makehuman_gameskel|gun_game_2_sholder", -1, 0); // 팔운동2 애니메이션 실행
+                        snow1 = true;
+                        snow0 = false;
+                       count1++;
+                       decision++; // 팔운동2 애니메이션이 실행되면 팔운동3 애니메이션이 실행될수있도록 decision = 2로 바꿔준다.
+                   }
+               }
+               }
+
+           else { }
+
+             print("카운트값2: " + decision);
+
+            print("2번" + snow1);
+
+            if (time >= 3.5f)
+            {
+                if (decision == 2)  // 팔운동3
+                {
+                    if (Math.Abs(_curRotation.z) < 0.4f ) // 2번 z: 현재값 < 0.4f
+                        
+
+                    {
+
+                        //animator.Play("Makehuman_gameskel|gun_game_4_win", -1, 0); // 팔운동3 애니메이션 실행
+                        snow2 = true;
+                        snow1 = false;
+                        count1++;
+                        decision = 0; // 팔운동3 애니메이션이 실행되면 팔운동1 애니메이션이 실행될수있도록 decision = 0 으로 바꿔준다.
+
+                    }
+                }
+            }
+
+            else { }
+            print("3번"+ snow2);
+
+            
+            // print("카운트값3: " + decision);
+
+           
+
+          
+
+
+
+           
+
+
+
+
+
+
+            if (count1 > 0)   //다리운동할때는 주석처리 / 허리와 팔은 필요함 
+            {
+                time = 0.0f;
+                
+                count1 = 0;
+            }
+            print(time);
+            
+
+        }
+
+
+
+
+
+
+
+
+        private void OnRotationChanged(Quaternion q)
+        {
+            _fromRotation = _toRotation;
+			_toRotation =  q;
+            
+            _time = 0f;
+        }
+		
+		protected override void AddNode(List<Node> nodes)
+        {
+			base.AddNode(nodes);
+			
+			nodes.Add(new Node("rotation", "Rotation", typeof(IWireInput<Quaternion>), NodeType.WireFrom, "Input<Quaternion>"));
+        }
+        
+        protected override void UpdateNode(Node node)
+        {
+            if(node.name.Equals("rotation"))
+            {
+                node.updated = true;
+                if(node.objectTarget == null && _rotation == null)
+                    return;
+                
+                if(node.objectTarget != null)
+                {
+                    if(node.objectTarget.Equals(_rotation))
+                        return;
+                }
+                
+                if(_rotation != null)
+                    _rotation.OnWireInputChanged -= OnRotationChanged;
+                
+                _rotation = node.objectTarget as IWireInput<Quaternion>;
+                if(_rotation != null)
+                    _rotation.OnWireInputChanged += OnRotationChanged;
+                else
+                    node.objectTarget = null;
+                
+                return;
+            }
+            
+            base.UpdateNode(node);
+        }
+
+
+	}
+}
