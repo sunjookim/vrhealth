15125d8b115822bd33353652defc2941923d2ce1
내컴\DooHyun <kdh9426@naver.com>
2018-08-30 04:35:20 +0900

    복구

---
+++ Assets/gameSleigh/Scripts/FollowCam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public GameObject Target;
+    public float dist = 10f;
+    public float height= 5f;
+    public float smoothRotate = 5.0f;
+
+    Vector3 cameraPo;
+    private void LateUpdate()
+    {
+        float currYAngle = Mathf.LerpAngle(transform.eulerAngles.y, Target.transform.eulerAngles.y, smoothRotate * Time.deltaTime);
+
+        Quaternion rot = Quaternion.Euler(0, currYAngle, 0);
+
+        transform.position = Target.transform.position - (rot * Vector3.forward * dist) + (Vector3.up * height);
+
+        transform.LookAt(Target.transform);
+        //float x = transform.rotation.x;
+        transform.Rotate(-20, 0, 0);
+    }
+}
---
+++ Assets/gameSleigh/Scripts/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// gameUI오브젝트에서 실행함
+public class Movie : MonoBehaviour { 
+
+    // 처음 시작 시 초기화 영상 재생
+    void Start()
+    {
+        GameObject obj = Instantiate(Resources.Load("Initializing")) as GameObject; // 초기화 영상 재생
+    }
+}
